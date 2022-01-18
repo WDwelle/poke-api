1d14521a039c60ba3d6570d1a90b66cf8dadd552
willd <willdwelle@frontier.com>
2022-01-18 14:26:23 -0800

    axiom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  //===vanilla js===
 //===useState set===
   const [poke, setPoke] = useState([])
-//===fetch method for API===
-  const fetchPoke = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=200")
+// //===fetch method for API===
+//   const fetchPoke = () => {
+//     fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=200")
+//       .then( res => {
+//         return res.json()
+//       } )
+//     .then(jsonPass => {
+//       console.log(jsonPass);//pass
+//       setPoke(jsonPass.results);
+//     })
+//     .catch( fail => console.log(fail) )//fail
+//   }
+
+//===axios===
+  const axiosPoke = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=200")
       .then( res => {
-        return res.json()
+        console.log(res.data.results)
+        setPoke(res.data.results)
       } )
-    .then(jsonPass => {
-      console.log(jsonPass);//pass
-      setPoke(jsonPass.results);
-    })
-    .catch( fail => console.log(fail) )//fail
+      .catch( err => console.log(err) )
   }
 
 
   return (
     <div className="App">
       <h1>Pokemon</h1>
-      <button onClick={fetchPoke}>fetch pokemon</button>
-      <hr />
+      {/*<button onClick={fetchPoke}>fetch pokemon</button>*/}
+      <button onClick={axiosPoke}>fetch pokemon</button>
       <table>
         <thead>
           <tr>
@@ -33,7 +45,7 @@ function App() {
           {
             poke.map((poke, idx) => {
               return(
-                <tr>
+                <tr key={poke.idx}>
                   <td>{poke.name}</td>
                 </tr>
               )
